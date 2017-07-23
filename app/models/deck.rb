3daa91d77708120d83cd4fb4a7db0b51c0e33ac9
Curtis <jacksonc4@mymail.vcu.edu>
2017-07-23 19:43:06 -0400

    Updated Styling

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,4 +1,7 @@
 class Deck < ApplicationRecord
     belongs_to :user
     has_many :comments
+    validates :title, presence: true
+    validates :format, presence: true
+    validates :description, presence: true
 end
